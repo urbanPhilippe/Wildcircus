5e6d64e8dcef2860733602c01a64c33345dbd0f5
philippe <spartitus>
2020-01-29 21:19:33 +0100

    show performances cards

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,18 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Performance;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/wild", name="wild")
+ */
 
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild")
+     * @Route("/", name="_index")
      */
     public function index()
     {
         return $this->render('wild/index.html.twig', [
             'controller_name' => 'WildController',
         ]);
+    }
+
+    /**
+     * Show all rows from Performance’s entity
+     *
+     * @Route("/performance", name="_performance")
+     */
+    public function showPerformance(): Response
+    {
+        $performance = $this->getDoctrine()
+            ->getRepository(Performance::class)
+            ->findAll();
+        if (!$performance) {
+            throw $this->createNotFoundException(
+                'No program found in performance\'s table.'
+            );
+        }
+        return $this->render(
+            'wild/performance.html.twig',
+            ['performance' => $performance]
+        );
     }
 }
